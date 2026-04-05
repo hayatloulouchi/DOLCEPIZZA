31d4892dafee581a42b01da1038b9df993ecdfd3
hayatloulouchi <loulouchih@gmail.com>
2026-04-05 19:16:03 +0100

    Ajout login admin + total commande + améliorations UI

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,19 @@
 function commander(nom) {
-  localStorage.setItem("pizza", nom);
+  localStorage.setItem("pizzaChoisie", nom);
   window.location.href = "commande.html";
+}
+
+const prix = {
+    "Margherita": 45,
+    "Pepperoni": 55,
+    "Quattro Stagioni": 60
+};
+function togglePassword() {
+  const input = document.getElementById("password");
+
+  if (input.type === "password") {
+    input.type = "text";
+  } else {
+    input.type = "password";
+  }
 }
